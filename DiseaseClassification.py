2fbb7b0b3f2b22815f3bf0ea87da0cca12f0c21b
ThePilot916 <shreyasmiraj8@gmail.com>
2018-11-24 11:10:42 +0530

    what was that bs

---
+++ DiseaseClassification.py
@@ -0,0 +1,108 @@
+
+# coding: utf-8
+
+# In[1]:
+
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense
+from keras import backend as K
+import os
+import tensorflow as tf
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+
+# In[2]:
+
+def predictValue():
+
+    img_width, img_height = 210, 210
+    epochs = 10
+    batch_size = 32
+
+    if K.image_data_format() == 'channels_first':
+        input_shape = (3, img_width, img_height)
+    else:
+        input_shape = (img_width, img_height, 3)
+
+
+    # In[3]:
+
+
+
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), input_shape=(210,210,3)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(32, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(Conv2D(64, (3, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Dropout(0.25))
+
+    model.add(Flatten())
+    model.add(Dense(512))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+
+    model.add(Dense(5))
+    model.add(Activation('softmax'))
+
+
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam',
+                  metrics=['accuracy'])
+    model.summary()
+
+    train_datagen = ImageDataGenerator(
+        rescale=1. / 255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=False)
+
+    # this is the augmentation configuration we will use for testing
+    test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+
+
+    model.load_weights("model/Final.h5")
+    model._make_predict_function()
+
+    # In[6]:
+
+
+    classList = ['corn gray leaf spot', 'corn common rust', 'corn healthy', 'peach bacterial spot', 'peach healthy']
+
+
+
+    datagen = ImageDataGenerator(rescale=1. / 255)
+    generator = datagen.flow_from_directory(
+            'imagedata',
+            target_size=(img_width, img_height),
+            batch_size=batch_size,
+            class_mode='categorical',  # only data, no labels
+            shuffle=False)  # keep data in same order as labels
+
+    op = model.predict_generator(generator,1)
+    maxprobability = max(op[0])
+
+    for i in range(len(op[0])):
+        if maxprobability == op[0][i]:
+            maxprobabilityclass = i
+            break
+
+    K.clear_session()
+    del model
+    if maxprobability < 0.5:
+        return str("image is not discernible")
+    return str(classList[maxprobabilityclass])
--- backend.py
+++ backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for,jsonify,request,flash,abort,render_template
+from flask import Flask, flash, request, redirect, url_for, render_template, json, session,jsonify,abort
 from flask_cors import CORS, cross_origin
 from flask_pymongo import PyMongo
 from bson.json_util import dumps
@@ -6,6 +6,15 @@ from bson.objectid import ObjectId
 import os
 import json
 import pyotp
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+from DiseaseClassification import *
+import shutil
+import glob
+
+
+UPLOAD_FOLDER = 'imagedata/result'
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
 
 app = Flask(__name__)
@@ -15,23 +24,15 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config["MONGO_URI"] = "mongodb://localhost:27017/disease_detection"
 mongo = PyMongo(app)
 
-@app.route('/')
-def launch():
-    return render_template('login.html')
 
-@app.route('/signup')
-def signup():
-    return render_template('signup.html')
 
 @app.route('/checkuser',methods=['POST'])
-def check_user():
-    data=request.get_json()
-    print(data)
-    user1=mongo.db.users.find_one({'email':data['email']})
+def check_user(email):
+    user1=mongo.db.users.find_one({'email':email})
     if user1:
-        return jsonify({'result':"Already registered"})
+        return 1
     else:
-        return jsonify({'result':"New User"})
+        return 0
 
 @app.route('/adduser',methods=['POST'])
 def adduser():
@@ -39,6 +40,8 @@ def adduser():
     email = request.form['email']
     password = request.form['password']
     location = request.form['location']
+    if(check_user(email) == 1):
+        return render_template('login.html',data="already registered")
     print(name)
     userdata = {'name':name ,'password':password,'email':email,'location':location}
     user=mongo.db.users.insert_one(userdata)
@@ -55,22 +58,99 @@ def adduser():
 
 @app.route('/auth',methods=['POST'])
 def auth_user():
-    data=request.get_json()
+    email=request.form['email']
+    password=request.form['password']
     query={
-		'email':data['email'] ,
-		'password':data['password']
+		'email':email,
+		'password':password
 	}
     #return jsonify(query)
     user=mongo.db.users.find_one(query)
-    user1=mongo.db.users.find_one({'email':data['email']})
+    user1=mongo.db.users.find_one({'email':email})
     if user:
         user['_id']=str(user['_id'])
         session['logged_in'] = True
-        return jsonify({'user_id':user['_id'],'uname':user['name'],'result':'Success','is_student':user['is_student']})
+        session['user_name'] = user['name']
+        session['user_id'] = user['_id']
+
+        return render_template('dashboard.html')
+        return jsonify({'user_id':user['_id'],'uname':user['name'],'result':'Success'})
     elif user1:
         return jsonify({'result':"failure"})
     else:
         return jsonify({'result':"failure"})
 
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/dash_render')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/user_render')
+def user():
+    return render_template('user.html')
+
+@app.route('/rss_render')
+def rss():
+    return render_template('rss.html')
+
+@app.route('/consult_render')
+def consult():
+    return render_template('consult.html')
+
+def allowed_file(filename):
+	return '.' in filename and \
+		   filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/uploadajax', methods=['GET', 'POST'])
+def uploadajax():
+	if request.method == 'POST':
+		# check if the post request has the file part
+		if 'file' not in request.files:
+			flash('No file part')
+			print("file not in request.files")
+			return redirect(request.url)
+		file = request.files['file']
+		# if user does not select file, browser also
+		# submit an empty part without filename
+		if file.filename == '':
+			flash('No selected file')
+			print("no selected files")
+			return redirect(request.url)
+		if file and allowed_file(file.filename):
+			filename = secure_filename(file.filename)
+			files = glob.glob(str(app.config['UPLOAD_FOLDER'])+'/*')
+			for f in files:
+				os.remove(f)
+			file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+			classValue = predictValue()
+			print(classValue)
+			session['classValue'] = classValue
+			params = {'classValue' : classValue, 'div1status' : 'none', 'div2status' : 'block'}
+			return jsonify(params)
+
+
+@app.route('/consult', methods=['GET', 'POST'])
+def uploadForm():
+	classValue = 'NA'
+	if 'classValue' in session:
+		classValue = session['classValue']
+	return render_template("upload.html", classValue = classValue, div1status="block", div2status="none")
+
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+	return send_from_directory(app.config['UPLOAD_FOLDER'],
+							   filename)
+
+
 if __name__ == '__main__':
-   app.run(debug = True)
+	app.secret_key = 'super secret key'
+	app.run(debug="true", threaded=True)
