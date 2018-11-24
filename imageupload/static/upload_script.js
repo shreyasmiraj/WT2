1db1ecfd154116510713b71e6626e06a0e91dfbd
BolshackLancer <shardulsingh1@gmail.com>
2018-11-24 08:52:44 +0530

    dynamic text changes

--- imageupload/static/upload_script.js
+++ imageupload/static/upload_script.js
@@ -1,6 +1,7 @@
 $(function() {
     $('#upload-file-btn').click(function() {
         var form_data = new FormData($('#upload-file')[0]);
+
         $.ajax({
             type: 'POST',
             url: '/uploadajax',
@@ -8,7 +9,30 @@ $(function() {
             contentType: false,
             cache: false,
             processData: false,
-            success: function(data) {
+            success: function(data, req, resp) {
+                $("#result-image-div").css("display", "block")
+                $("#upload-image-div").css("display", "none")
+                $('#classValue').html("The given image is a " + resp["responseJSON"]["classValue"])
+                console.log('Success!');
+            },
+        });
+    });
+});
+
+
+$(function() {
+    $('#upload-more-btn').click(function() {
+ 
+        $("#result-image-div").css("display", "none")
+        $("#upload-image-div").css("display", "block")
+         $.ajax({
+            type: 'POST',
+            url: '/',
+            contentType: false,
+            cache: false,
+            processData: false,
+            success: function(data, req, resp) {
+
                 console.log('Success!');
             },
         });
